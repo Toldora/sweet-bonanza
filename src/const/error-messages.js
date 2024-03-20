797c9dd8e84a6177dcf740956ea4ecbf987d6781
Aleksandr Kropivnyy <kropivnyy@gmail.com>
2024-03-20 14:39:03 +0200

    added sms auth

--- src/const/error-messages.js
+++ src/const/error-messages.js
@@ -1,4 +1,6 @@
 export const ERROR_MESSAGES = {
   invalidEmail: 'The email must be a valid email address.',
-  playerExist: 'This email is already in use. Please enter a different one.',
+  emailExist: 'This email is already in use. Please enter a different one.',
+  phoneExist: 'This phone is already in use. Please enter a different one.',
+  invalidPhone: 'Por favor, use um número de telefone existente',
 };
